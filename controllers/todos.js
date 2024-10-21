6d4d5d950bc4eaf3b63a59ebee4c6cb9d0af3c7f
F1Zuwu <113283931+F1Zuwu@users.noreply.github.com>
2024-10-21 09:21:20 +0300

    Update the Todo by id

--- controllers/todos.js
+++ controllers/todos.js
@@ -20,6 +20,25 @@ class todoController {
     getTodos(req, res) {
         res.json({tasks: this.TODOS})
     }
+
+    updateTodo(req,res) {
+        const todoId = req.params.id
+        const updatedTask = req.body.task
+        const todoIndex = this.TODOS.findIndex((todo) => todo.id === todoId)
+
+        if(todoIndex < 0) {
+            res.json({
+                message:"Could not find todo with such index"
+            })
+            throw new Error("Could not find todo!")
+        }
+
+        this.TODOS[todoIndex] = new Todo(this.TODOS[todoIndex].id, updatedTask)
+        res.json({
+            message: "updated todo",
+            updatedTask: this.TODOS[todoIndex]
+        })
+    }
 }
 
 export const TodoController = new todoController()
